16305aae33c93a886072c9c8ae8fdef216faec6f
John Kitchin <kitchin@google.com>
2018-01-23 13:19:02 -0800

    add tf utilities and tests

---
+++ dap/tests/test_tf_utils.py
@@ -0,0 +1,215 @@
+"""Tests for tensorflow module."""
+import itertools
+import numpy as np
+import tensorflow as tf
+
+from dap.tf.utils import (tri, triu_indices, tril_indices, triu_indices_from,
+                          tril_indices_from, combinations)
+
+
+class TestTFUtils_tri(tf.test.TestCase):
+
+  def test_tri(self):
+    npt = np.tri(3, dtype=np.bool)
+    tft = tri(3)
+    with self.test_session():
+      self.assertTrue(np.all(npt == tft.eval()))
+
+  def test_above(self):
+    npt = np.tri(3, k=1, dtype=np.bool)
+    tft = tri(3, k=1)
+    with self.test_session():
+      self.assertTrue(np.all(npt == tft.eval()))
+
+  def test_below(self):
+    npt = np.tri(3, k=-1, dtype=np.bool)
+    tft = tri(3, k=-1)
+    with self.test_session():
+      self.assertTrue(np.all(npt == tft.eval()))
+
+  def test_notsquare(self):
+    npt = np.tri(3, 4, dtype=np.bool)
+    tft = tri(3, 4)
+    with self.test_session():
+      self.assertTrue(np.all(npt == tft.eval()))
+
+  def test_notsquare_above(self):
+    npt = np.tri(3, 4, k=1, dtype=np.bool)
+    tft = tri(3, 4, k=1)
+    with self.test_session():
+      self.assertTrue(np.all(npt == tft.eval()))
+
+  def test_notsquare_below(self):
+    npt = np.tri(3, 4, k=-1, dtype=np.bool)
+    tft = tri(3, 4, k=-1)
+    with self.test_session():
+      self.assertTrue(np.all(npt == tft.eval()))
+
+
+class TestTFUtils_triu(tf.test.TestCase):
+
+  def test_triu(self):
+    npu = np.triu_indices(3)
+    r0, r1 = triu_indices(3)
+    with self.test_session():
+      self.assertTrue(np.all(npu[0] == r0.eval()))
+      self.assertTrue(np.all(npu[1] == r1.eval()))
+
+  def test_triu_k_over(self):
+    npu = np.triu_indices(3, k=1)
+    r0, r1 = triu_indices(3, k=1)
+    with self.test_session():
+      self.assertTrue(np.all(npu[0] == r0.eval()))
+      self.assertTrue(np.all(npu[1] == r1.eval()))
+
+  def test_triu_k_under(self):
+    npu = np.triu_indices(3, k=-1)
+    r0, r1 = triu_indices(3, k=-1)
+    with self.test_session():
+      self.assertTrue(np.all(npu[0] == r0.eval()))
+      self.assertTrue(np.all(npu[1] == r1.eval()))
+
+  def test_triu_nonsquare(self):
+    npu = np.triu_indices(3, m=4)
+    r0, r1 = triu_indices(3, m=4)
+    with self.test_session():
+      self.assertTrue(np.all(npu[0] == r0.eval()))
+      self.assertTrue(np.all(npu[1] == r1.eval()))
+
+  def test_triu_nonsquare_long(self):
+    npu = np.triu_indices(3, m=2)
+    r0, r1 = triu_indices(3, m=2)
+    with self.test_session():
+      self.assertTrue(np.all(npu[0] == r0.eval()))
+      self.assertTrue(np.all(npu[1] == r1.eval()))
+
+
+class TestTFUtils_tril(tf.test.TestCase):
+
+  def test_tril(self):
+    npu = np.tril_indices(3)
+    r0, r1 = tril_indices(3)
+    with self.test_session():
+      self.assertTrue(np.all(npu[0] == r0.eval()))
+      self.assertTrue(np.all(npu[1] == r1.eval()))
+
+  def test_tril_k_over(self):
+    npu = np.tril_indices(3, k=1)
+    r0, r1 = tril_indices(3, k=1)
+    with self.test_session():
+      self.assertTrue(np.all(npu[0] == r0.eval()))
+      self.assertTrue(np.all(npu[1] == r1.eval()))
+
+  def test_tril_k_under(self):
+    npu = np.tril_indices(3, k=-1)
+    r0, r1 = tril_indices(3, k=-1)
+    with self.test_session():
+      self.assertTrue(np.all(npu[0] == r0.eval()))
+      self.assertTrue(np.all(npu[1] == r1.eval()))
+
+  def test_tril_nonsquare(self):
+    npu = np.tril_indices(3, m=4)
+    r0, r1 = tril_indices(3, m=4)
+    with self.test_session():
+      self.assertTrue(np.all(npu[0] == r0.eval()))
+      self.assertTrue(np.all(npu[1] == r1.eval()))
+
+  def test_tril_nonsquare_long(self):
+    npu = np.tril_indices(3, m=2)
+    r0, r1 = tril_indices(3, m=2)
+    with self.test_session():
+      self.assertTrue(np.all(npu[0] == r0.eval()))
+      self.assertTrue(np.all(npu[1] == r1.eval()))
+
+
+class TestTFUtils_triu_indices_from(tf.test.TestCase):
+
+  def test_triu_indices_from(self):
+
+    a = np.zeros((3, 3))
+    ref1, ref2 = np.triu_indices_from(a)
+
+    tref1, tref2 = triu_indices_from(a)
+    with self.test_session():
+      self.assertTrue(np.all(ref1 == tref1.eval()))
+      self.assertTrue(np.all(ref2 == tref2.eval()))
+
+  def test_triu_indices_from_kover(self):
+
+    a = np.zeros((3, 3))
+    ref1, ref2 = np.triu_indices_from(a, k=1)
+
+    tref1, tref2 = triu_indices_from(a, k=1)
+    with self.test_session():
+      self.assertTrue(np.all(ref1 == tref1.eval()))
+      self.assertTrue(np.all(ref2 == tref2.eval()))
+
+  def test_triu_indices_from_kunder(self):
+
+    a = np.zeros((3, 3))
+    ref1, ref2 = np.triu_indices_from(a, k=-1)
+    tref1, tref2 = triu_indices_from(a, k=-1)
+    with self.test_session():
+      self.assertTrue(np.all(ref1 == tref1.eval()))
+      self.assertTrue(np.all(ref2 == tref2.eval()))
+
+  def test_triu_indices_from_non2d(self):
+    a = np.zeros((3, 3, 3))
+    with self.test_session():
+      with self.assertRaises(ValueError):
+        triu_indices_from(a)
+
+
+class TestTFUtils_tril_indices_from(tf.test.TestCase):
+
+  def test_tril_indices_from(self):
+
+    a = np.zeros((3, 3))
+    ref1, ref2 = np.tril_indices_from(a)
+
+    tref1, tref2 = tril_indices_from(a)
+    with self.test_session():
+      self.assertTrue(np.all(ref1 == tref1.eval()))
+      self.assertTrue(np.all(ref2 == tref2.eval()))
+
+  def test_tril_indices_from_kover(self):
+
+    a = np.zeros((3, 3))
+    ref1, ref2 = np.tril_indices_from(a, k=1)
+
+    tref1, tref2 = tril_indices_from(a, k=1)
+    with self.test_session():
+      self.assertTrue(np.all(ref1 == tref1.eval()))
+      self.assertTrue(np.all(ref2 == tref2.eval()))
+
+  def test_tril_indices_from_kunder(self):
+
+    a = np.zeros((3, 3))
+    ref1, ref2 = np.tril_indices_from(a, k=-1)
+    tref1, tref2 = tril_indices_from(a, k=-1)
+    with self.test_session():
+      self.assertTrue(np.all(ref1 == tref1.eval()))
+      self.assertTrue(np.all(ref2 == tref2.eval()))
+
+  def test_tril_indices_from_non2d(self):
+    a = np.zeros((3, 3, 3))
+    with self.test_session():
+      with self.assertRaises(ValueError):
+        tril_indices_from(a)
+
+
+class TestTFUtils_combinations(tf.test.TestCase):
+
+  def test_combinations_2(self):
+    a = [0, 1, 2, 3, 4]
+    for k in [2, 3]:
+      combs = np.array(list(itertools.combinations(a, k)))
+      with self.test_session():
+        tf_combs = combinations(a, k).eval()
+        self.assertTrue(np.all(combs == tf_combs))
+
+  def test_combinations_non1d(self):
+    a = [[0, 1, 2, 3, 4]]
+    with self.assertRaises(ValueError):
+      with self.test_session():
+        tf_combs = combinations(a, 2).eval()
---
+++ dap/tf/utils.py
@@ -0,0 +1,129 @@
+"""Tensorflow utilities.
+These are mostly functions that bring additional numpy functionality to
+Tensorflow.
+"""
+import itertools
+import numpy as np
+import tensorflow as tf
+
+
+def tri(N, M=None, k=0, dtype=tf.float64):
+  """
+    An array with ones at and below the given diagonal and zeros elsewhere.
+    Parameters
+    ----------
+    N : int
+        Number of rows in the array
+    M : int, optional
+        Number of columns in the array. Defaults to number of rows.
+    k : The subdiagonal at and below which the array is filled, optional
+    Returns
+    -------
+    out : tensor of shape (N, M)
+    Modeled after pydoc:numpy.tri.
+    """
+
+  if M is None:
+    M = N
+
+  r1 = tf.range(N)
+  r2 = tf.range(-k, M - k)
+  return tf.greater_equal(r1[:, None], r2[None, :])
+
+
+def triu_indices(n, k=0, m=None):
+  """Return indices for upper triangle of an (n, m) array.
+    Parameters
+    ----------
+    n : int
+      number of rows in the array.
+    k : int, optional
+      diagonal offset.
+    m : int, optional
+      number of columns in the array. Defaults to `n`.
+    Returns
+    -------
+    inds : a tensor, shape = (None, 2)
+      column 0 is one set of indices, column 1 is the other set.
+    modeled after pydoc:numpy.triu_indices.
+    """
+
+  result = tf.where(tf.logical_not(tri(n, m, k=k - 1)))
+  return result[:, 0], result[:, 1]
+
+
+def triu_indices_from(arr, k=0):
+  """Return the indices for the upper-triangle of arr.
+    Parameters
+    ----------
+    arr : tensor or array.
+    k : diagonal.
+    see pydoc:numpy.triu_indices_from.
+    """
+  tensor = tf.convert_to_tensor(arr)
+  shape = tensor.get_shape().as_list()
+  if len(shape) != 2:
+    raise ValueError("Tensor must be 2d")
+  return triu_indices(shape[-2], k=k, m=shape[-1])
+
+
+def tril_indices(n, k=0, m=None):
+  """Return indices for lower triangle of an (n, m) array.
+    Parameters
+    ----------
+    n : int
+      number of rows in the array.
+    k : int, optional
+      diagonal offset.
+    m : int, optional
+      number of columns in the array. Defaults to `n`.
+    Returns
+    -------
+    inds : a tensor, shape = (None, 2)
+      column 0 is one set of indices, column 1 is the other set.
+    modeled after pydoc:numpy.tril_indices.
+    """
+
+  result = tf.where(tri(n, m, k=k))
+  return result[:, 0], result[:, 1]
+
+
+def tril_indices_from(arr, k=0):
+  """Return the indices for the lower-triangle of arr.
+    Parameters
+    ----------
+    arr : tensor or array.
+    k : diagonal.
+    see pydoc:numpy.tril_indices_from.
+    """
+  tensor = tf.convert_to_tensor(arr)
+  shape = tensor.get_shape().as_list()
+  if len(shape) != 2:
+    raise ValueError("Tensor must be 2d")
+  return tril_indices(shape[-2], k=k, m=shape[-1])
+
+
+def combinations(arr, k):
+  """Return tensor of combinations of k elements.
+    Parameters
+    ----------
+    arr : 1D array or tensor
+    k : number of elements to make combinations of .
+    Returns
+    -------
+    a 2D tensor of combinations. Each row is a combination, and each element of
+    the combination is in the columns.
+    Related: pydoc:itertools.combinations
+    """
+  tensor = tf.convert_to_tensor(arr)
+
+  shape = tensor.get_shape().as_list()
+  if len(shape) != 1:
+    raise ValueError("Tensor must be 1d")
+
+  N = shape[0]
+  inds = np.arange(N)
+  combination_indices = [
+      combination for combination in itertools.combinations(inds, k)
+  ]
+  return tf.stack([tf.gather(arr, ind) for ind in combination_indices])
