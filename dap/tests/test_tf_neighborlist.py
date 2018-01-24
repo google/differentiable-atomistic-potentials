a9cd17c493451cbba48b49d4d7dbeafb763b6c5d
John Kitchin <kitchin@google.com>
2018-01-23 17:13:35 -0800

    add nieghborlist code and tests.

---
+++ dap/tests/test_tf_neighborlist.py
@@ -0,0 +1,112 @@
+# Copyright 2018 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tests for the tensorflow neighborlist module.
+
+pydoc:dap.tf.neighborlist
+"""
+
+import numpy as np
+import tensorflow as tf
+from ase.build import bulk
+from ase.neighborlist import NeighborList
+from dap.tf.neighborlist import (get_distances)
+
+
+class TestNeighborlist(tf.test.TestCase):
+  """Tests comparing the TF version and ASE neighborlist implementations.
+
+  """
+
+  def test_basic(self):
+    a = 3.6
+    Rc = a / np.sqrt(2) / 2
+    atoms = bulk('Cu', 'fcc', a=a)
+
+    nl = NeighborList(
+        [Rc] * len(atoms), skin=0.01, self_interaction=False, bothways=True)
+    nl.update(atoms)
+
+    distances = get_distances({
+        'cutoff_radius': 2 * Rc
+    }, atoms.positions, atoms.cell, np.ones((len(atoms), 1)))
+
+    mask = (distances <= 2 * Rc) & (distances > 0)
+    tf_nneighbors = tf.reduce_sum(tf.cast(mask, tf.int32), axis=[1, 2])
+
+    with self.test_session():
+      for i, atom in enumerate(atoms):
+        inds, disps = nl.get_neighbors(i)
+        ase_nneighbors = len(inds)
+        self.assertEqual(ase_nneighbors, tf_nneighbors.eval()[i])
+
+  def test_structure_repeats(self):
+    'Check several structures and repeats for consistency with ase.'
+    for repeat in ((1, 1, 1), (2, 1, 1), (1, 2, 1), (1, 1, 2), (1, 2, 3)):
+      for structure in ('fcc', 'bcc', 'sc', 'hcp', 'diamond'):
+        a = 3.6
+        # Float tolerances are tricky. The 0.01 in the next line is important.
+        # This test fails without it due to subtle differences in computed
+        # positions.
+        Rc = 2 * a + 0.01
+        atoms = bulk('Cu', structure, a=a).repeat(repeat)
+        nl = NeighborList(
+            [Rc] * len(atoms), skin=0.0, self_interaction=False, bothways=True)
+        nl.update(atoms)
+        distances = get_distances({
+            'cutoff_radius': 2 * Rc
+        }, atoms.positions, atoms.cell, np.ones((len(atoms), 1)))
+
+        mask = (distances <= 2 * Rc) & (distances > 0)
+        tf_nneighbors = tf.reduce_sum(tf.cast(mask, tf.int32), axis=[1, 2])
+
+        with self.test_session():
+          for i, atom in enumerate(atoms):
+            inds, disps = nl.get_neighbors(i)
+            ase_nneighbors = len(inds)
+            self.assertEqual(ase_nneighbors, tf_nneighbors.eval()[i])
+
+            # These are the indices of each neighbor in the atom list.
+            tf_inds = tf.where(mask[i])[:, 0].eval()
+            self.assertCountEqual(inds, tf_inds)
+
+  def test_atom_types(self):
+    """Tests if the neighbor indices agree with ase.
+
+    This is important to find the
+    chemical element associated with a specific neighbor.
+
+    """
+    a = 3.6
+    Rc = a / np.sqrt(2) / 2 + 0.01
+
+    atoms = bulk('Cu', 'fcc', a=a).repeat((3, 1, 1))
+    atoms[1].symbol = 'Au'
+
+    nl = NeighborList(
+        [Rc] * len(atoms), skin=0.01, self_interaction=False, bothways=True)
+    nl.update(atoms)
+    nns = [nl.get_neighbors(i) for i in range(len(atoms))]
+    ase_nau = [np.sum(atoms.numbers[inds] == 79) for inds, offs in nns]
+
+    au_mask = tf.convert_to_tensor(atoms.numbers == 79, tf.int32)
+
+    distances = get_distances({
+        'cutoff_radius': 2 * Rc
+    }, atoms.positions, atoms.cell)
+    mask = (distances <= (2 * Rc)) & (distances > 0)
+
+    nau = tf.reduce_sum(tf.cast(mask, tf.int32) * au_mask[:, None], [1, 2])
+
+    with self.test_session():
+      self.assertTrue(np.all(ase_nau == nau.eval()))
---
+++ dap/tf/neighborlist.py
@@ -0,0 +1,122 @@
+# Copyright 2018 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Neighborlist functions for tensorflow."""
+
+import tensorflow as tf
+
+
+def get_distances(config, positions, cell, atom_mask=None):
+  """Get distances to neighboring atoms with periodic boundary conditions.
+
+  The way this function works is it tiles a volume with unit cells to at least
+  fill a sphere with a radius of cutoff_radius. That means some atoms will be
+  outside the cutoff radius. Those are included in the results. Then we get
+  distances to all atoms in the tiled volume. This is always the same number for
+  every atom, so we have consistent sized arrays.
+
+  Args:
+    config: A dictionary containing 'cutoff_radius' with a float value.
+    positions: array-like or Tensor shape=(numatoms, 3)
+      Array of cartesian coordinates of atoms in a unit cell.
+    cell: array-like shape=(3, 3)
+      Array of unit cell vectors in cartesian basis. Each row is a unit cell
+      vector.
+    atom_mask: array-like (numatoms,)
+      ones for atoms, zero for padded positions. If None, defaults to all ones
+    cutoff_radius: float
+      The cutoff_radius we want atoms within.
+
+  Returns:
+    distances: shape=(maxnatoms, maxnatoms, nunitcells) containing the distances
+    between all pairs of atoms in the tiled volume.
+
+  Related
+  -------
+
+  pydoc:pymatgen.core.lattice.Lattice.get_points_in_sphere
+
+  """
+  with tf.name_scope('get_distances'):
+    positions = tf.convert_to_tensor(positions)
+    cell = tf.convert_to_tensor(cell)
+
+    if atom_mask is None:
+      natoms = positions.get_shape()[0]
+      atom_mask = tf.ones((natoms, 1), dtype=cell.dtype)
+    else:
+      atom_mask = tf.convert_to_tensor(atom_mask, dtype=cell.dtype)
+    cutoff_radius = tf.convert_to_tensor(
+        config['cutoff_radius'], dtype=cell.dtype)
+    # Next we get the inverse unit cell, which will be used to compute the
+    # unit cell offsets required to tile space inside the sphere.
+    inverse_cell = tf.matrix_inverse(cell)
+
+    fractional_coords = tf.mod(
+        tf.matmul(positions, inverse_cell), tf.ones_like(positions))
+
+    num_cell_repeats = cutoff_radius * tf.norm(inverse_cell, axis=0)
+
+    mins = tf.reduce_min(tf.floor(fractional_coords - num_cell_repeats), axis=0)
+    maxs = tf.reduce_max(tf.ceil(fractional_coords + num_cell_repeats), axis=0)
+
+    # Now we generate a set of cell offsets. We start with the repeats in each
+    # unit cell direction.
+    v0_range = tf.range(mins[0], maxs[0])
+    v1_range = tf.range(mins[1], maxs[1])
+    v2_range = tf.range(mins[2], maxs[2])
+
+    # Then we expand them in each dimension
+    xhat = tf.constant([1.0, 0.0, 0.0], dtype=inverse_cell.dtype)
+    yhat = tf.constant([0.0, 1.0, 0.0], dtype=inverse_cell.dtype)
+    zhat = tf.constant([0.0, 0.0, 1.0], dtype=inverse_cell.dtype)
+
+    v0_range = v0_range[:, None] * xhat[None, :]
+    v1_range = v1_range[:, None] * yhat[None, :]
+    v2_range = v2_range[:, None] * zhat[None, :]
+
+    # And combine them to get an offset vector for each cell
+    offsets = (
+        v0_range[:, None, None] + v1_range[None, :, None] +
+        v2_range[None, None, :])
+
+    offsets = tf.reshape(offsets, (-1, 3))
+
+    # Now we have a vector of unit cell offsets (offset_index, 3) in the inverse
+    # unit cell basis. We convert that to cartesian coordinate offsets here.
+    cart_offsets = tf.matmul(offsets, cell)
+
+    # we need to offset each atom coordinate by each offset.
+    # This array is (atom_index, offset, 3)
+    shifted_cart_coords = positions[:, None] + cart_offsets[None, :]
+
+    # Next, we subtract each position from the array of positions.
+    # This leads to (atom_i, atom_j, positionvector, xhat)
+    relative_positions = shifted_cart_coords - positions[:, None, None]
+
+    # This is the distance squared. This leads to (atom_i, atom_j, distance2)
+    distances2 = tf.reduce_sum(relative_positions**2, axis=3)
+
+    # We zero out masked distances.
+    distances2 *= atom_mask
+    distances2 *= atom_mask[:, None]
+
+    # We do not mask out the values greater than cutoff_radius here. That is
+    # done later in the energy function. The zero masking here is due to the
+    # fact that the gradient of the square_root at x=0 is nan, so we have to
+    # avoid the zeros. Here we replace the zeros temporarily with ones, take the
+    # sqrt, and then return the right parts.
+    zeros = tf.equal(distances2, 0.0)
+    adjusted = tf.where(zeros, tf.ones_like(distances2), distances2)
+    distance = tf.sqrt(adjusted)
+    return tf.where(zeros, tf.zeros_like(distance), distance)
