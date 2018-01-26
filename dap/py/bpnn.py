8f0cf7356ef25c7199bd02b73fd307b2e943ebd4
John Kitchin <kitchin@google.com>
2018-01-26 15:01:21 -0800

    add py module with bpnn functions and test.
    the bpnn module is not yet complete.

---
+++ dap/py/bpnn.py
@@ -0,0 +1,99 @@
+# Copyright 2018 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import numpy as np
+
+
+def pad(array, shape):
+  """Returns a zero-padded array with shape."""
+  array = np.array(array)
+  p = np.zeros(shape)
+
+  r, c = array.shape
+  p[:r, :c] = array
+  return p
+
+
+def cosine_cutoff(config, distances, atom_mask=None):
+  """Cosine cutoff function.
+
+    Parameters
+    ----------
+
+    config: a dictionary containing 'cutoff_radius' as a float
+    distances: the distance array from get_distances
+    atom_mask: An array of ones for atoms, 0 for non-atoms. 
+      Defaults to all atoms.
+
+  """
+  cutoff_radius = config.get('cutoff_radius', 6.0)
+  distances = np.array(distances)
+
+  if atom_mask is None:
+    atom_mask = np.ones((len(distances), 1))
+  else:
+    atom_mask = np.array(atom_mask)
+
+  cc = 0.5 * (np.cos(np.pi * (distances / cutoff_radius)) + 1.0)
+  cc *= (distances <= cutoff_radius) & (distances > 0.0)
+  cc *= atom_mask
+  cc *= atom_mask[:, None]
+  return cc
+
+
+def G2(species_index, eta, Rs):
+  """G2 function generator.
+
+    This is a radial function between an atom and atoms with some chemical
+    symbol. It is defined in cite:behler-2011-atom-center Eq 6.
+
+    Parameters
+    ----------
+
+    species_index : integer
+      species index for this function. Elements that do not have this index will
+      be masked out
+
+    eta : float
+      The gaussian width
+
+    Rs : float
+      The gaussian center or shift
+
+    Returns
+    -------
+    The g2 function with the cosine_cutoff function integrated into it.
+
+  """
+
+  def g2(config, distances, atom_mask, species_masks):
+    distances = np.array(distances)
+    atom_mask = np.array(atom_mask)
+    species_masks = np.array(species_masks)
+    # Mask out non-species contributions
+    smask = species_masks[:, species_index][:, None]
+    distances *= smask
+    distances *= atom_mask
+    distances *= atom_mask[:, None]
+
+    Rc = config.get('cutoff_radius', 6.5)
+    result = np.where(distances > 0,
+                      np.exp(-eta * ((distances - Rs)**2 / Rc**2)), 0.0)
+
+    result *= cosine_cutoff(config, distances, atom_mask)
+    gsum = np.sum(result, (1, 2))
+    return gsum[:, None]
+
+  g2.__desc__ = 'g2({species_index}, eta={eta}, Rs={Rs})'.format(**locals())
+  return g2
---
+++ dap/tests/test_py_bpnn.py
@@ -0,0 +1,98 @@
+# Copyright 2018 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import unittest
+import numpy as np
+import numpy.testing as npt
+from ase.build import molecule
+from amp.descriptor.gaussian import *
+from amp.utilities import hash_images
+
+from dap.ag.neighborlist import get_distances
+from dap.py.bpnn import pad, cosine_cutoff, G2
+
+
+class TestPadding(unittest.TestCase):
+
+  def test0(self):
+    a = np.ones((2, 3))
+
+    padded_array = pad(a, (3, 3))
+
+    res = np.ones((3, 3))
+    res[2, :] = 0
+
+    self.assertTrue(np.all(padded_array == res))
+
+
+class TestG2(unittest.TestCase):
+  """Run comparisons of the python G2 functions against Amp."""
+
+  def setUp(self):
+    # amp saves these and we want to eliminate them
+    os.system('rm -fr amp-data*')
+
+  def tearDown(self):
+    os.system('rm -fr amp-data*')
+
+  def test_radial_G2(self):
+    atoms = molecule('H2O')
+    atoms.cell = 100 * np.eye(3)
+
+    # Amp setup
+
+    sf = {
+        'H': make_symmetry_functions(['H', 'O'], 'G2', [0.05, 1.0]),
+        'O': make_symmetry_functions(['H', 'O'], 'G2', [0.05, 1.0])
+    }
+
+    descriptor = Gaussian(Gs=sf)
+    images = hash_images([atoms], ordered=True)
+    descriptor.calculate_fingerprints(images)
+
+    fparray = []
+    for index, hash in enumerate(images.keys()):
+      for fp in descriptor.fingerprints[hash]:
+        fparray += [fp[1]]
+    fparray = np.array(fparray)
+
+    # This module setup
+    positions = atoms.positions
+    cell = atoms.cell
+    atom_mask = [[1] for atom in atoms]
+
+    numbers = list(np.unique(atoms.numbers))
+
+    species_mask = np.stack(
+        [[atom.number == el for atom in atoms] for el in numbers],
+        axis=1).astype(int)
+
+    config = {'cutoff_radius': 6.5}
+    d = get_distances(positions, cell, config['cutoff_radius'])
+
+    g0 = G2(0, 0.05, 0.0)
+    g1 = G2(1, 0.05, 0.0)
+    g2 = G2(0, 1.0, 0.0)
+    g3 = G2(1, 1.0, 0.0)
+
+    # This builds the array of fingerprints
+    this = np.concatenate(
+        (g0(config, d, atom_mask, species_mask),
+         g1(config, d, atom_mask, species_mask),
+         g2(config, d, atom_mask, species_mask),
+         g3(config, d, atom_mask, species_mask)),
+        axis=1)
+
+    npt.assert_almost_equal(fparray, this, 5)
